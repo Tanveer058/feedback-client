b959d2f0bf37647f1cc883b4ff9e701cc7b39e99
Tanveer058 <zardaritanveer058@gmail.com>
2025-10-25 18:11:09 +0500

    updated homepgae

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Navbar from '../components/Navbar';
 const Home = () => {
   return (
     <>
-      <Navbar />
       <Container maxWidth="md">
         <Box sx={{ textAlign: 'center', mt: 6, mb: 4 }}>
           <Typography variant="h4" component="h1" gutterBottom>
